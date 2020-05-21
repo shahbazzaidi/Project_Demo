16fe2783a58314808f63349ac6facd90622dd0ee
shahbazzaidi <shahbazzaidi0980@gmail.com>
2020-05-21 12:40:33 +0530

    First Change

---
+++ user/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+	
+
+
+
+
+def about(request):
+	return render(request, 'about.html')
+
+
+
+
+
+def signup(request):
+	return render(request, 'signup.html')
